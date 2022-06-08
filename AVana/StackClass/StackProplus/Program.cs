9dca288327c7918b2df1c53c1e4aa9f120f3efb6
Anusha Vana <anusha.vana@genzeon.com>
2022-06-08 17:21:39 +0530

    StackClass

---
+++ AVana/StackClass/StackProplus/Program.cs
@@ -0,0 +1,18 @@
+﻿DataStructure.Stack<int> stack1 = new DataStructure.Stack<int>();
+DataStructure.Stack<string> stack2 = new DataStructure.Stack<string>();
+stack1.Push(1);
+stack1.Push(5);
+stack1.Push(9);
+stack1.Push(10);
+int r = stack1.Pop();
+Console.WriteLine(r);
+stack2.Push("a");
+stack2.Push("b");
+stack2.Push("c");
+string g = stack2.Pop();
+Console.WriteLine(g);
+
+
+
+
+
