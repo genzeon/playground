0be4da3a89bbeb0c559f7297c772f9e07b748bd7
Sunitha Ramayanam <sunitha.ramayanam@genzeon.com>
2022-07-22 18:06:19 +0530

    adding Coinwebapp

---
+++ RSunitha/FlipingOfACoin/FlipingOfACoin/Models/Coin.cs
@@ -0,0 +1,32 @@
+﻿using FlipingOfACoin.Controllers;
+
+namespace FlipingOfACoin.Models
+{
+    public class Coin
+    {
+
+        face _upside;
+        face _downside;
+
+        public face Upside
+        {
+            get { return _upside; }
+            set { _upside = value; }
+        }
+
+        public face Downside
+        {
+            get { return _downside; }
+            set { _downside = value; }
+        }
+
+        public Coin()
+        {
+            Upside = face.Heads;
+            Downside = face.Tails;
+        }
+
+
+    }
+
+}
