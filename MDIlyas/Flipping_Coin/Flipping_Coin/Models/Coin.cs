7b3d0fc098e306b146475d8950c4bde703d750a4
Ilyas Taj <ilyas.taj@genzeon.com>
2022-07-22 17:58:08 +0530

    adding Coin Webapp

---
+++ MDIlyas/Flipping_Coin/Flipping_Coin/Models/Coin.cs
@@ -0,0 +1,58 @@
+﻿namespace Flipping_Coin.Models
+{
+  
+    class Coin
+    {
+       enum Sides
+        {
+            Heads, Tails
+        }
+
+        private Enum _up;
+        public Enum Up
+        {
+            get { return _up; }
+            set { _up = value; }
+        }
+
+        private Enum _down;
+        public Enum Down
+        {
+            get { return _down; }
+            set { _down = value; }
+        }
+
+        public void setInitialValues()
+        {
+            Up = Sides.Heads;
+            Down = Sides.Tails;
+        }
+
+        public override string ToString()
+        {
+            return $"Up side : {Up.ToString()} and the Down side : {Down.ToString()}";
+        }
+
+
+        public void Flip()
+        {
+            Random random = new Random();
+            int num = random.Next(2);
+            
+            if(num == 0)
+            {
+                Up = Sides.Heads;
+                Down = Sides.Tails;
+            }
+            else
+            {
+                Up = Sides.Tails;
+                Down = Sides.Heads;
+            }
+        }
+       
+
+    }
+
+  
+}
