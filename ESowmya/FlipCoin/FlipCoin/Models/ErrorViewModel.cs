3d719e5553c5f293012bdc16f0ffb37e39b391f7
Sowmya Eppalpalli <sowmya.eppalpalli@genzeon.com>
2022-07-22 18:04:19 +0530

    adding the web app

---
+++ ESowmya/FlipCoin/FlipCoin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlipCoin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
