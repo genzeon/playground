5940eeb80bd300e9ae42d06c97ad8a8bfeee080f
Ramu Ramisetty <ramu.ramisetty@genzeon.com>
2022-07-22 18:18:04 +0530

    coinflip

---
+++ RRamisetty/CoinFlip/CoinFlip/Flipping.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    public class coin
+    {
+        public enum face
+        {
+            Heads, Tails
+        }
+        face _upside;
+        face _downside;
+
+        public face upside
+        {
+            get { return _upside; }
+            set { _upside = value; }
+        }
+
+        public face downside
+        {
+            get { return _downside; }
+            set { _downside = value; }
+        }
+
+        public coin()
+        {
+            upside = face.Heads;
+            downside = face.Tails;
+        }
+
+        public override string ToString()
+        {
+            return $"Up{upside},Down{downside}";
+        }
+
+        public void Flip()
+        {
+            int  num;
+
+            Random r=new Random();
+
+            num = r.Next(2);
+            if (num == 0)
+            {
+                upside = face.Heads;
+                downside = face.Tails;
+            }
+            else
+            {
+                upside = face.Tails;
+                downside = face.Heads;
+                
+            
+            }
+        
+        }
+        
+
+
+      
+    
+    
+    
+    
+    }
+}
---
+++ RRamisetty/FlippingCoins/FlippingCoins/Models/Coin.cs
@@ -0,0 +1,31 @@
+﻿using FlippingCoins.Controllers;
+
+namespace FlippingCoins.Models
+{
+    public class Coin
+    {
+   
+        face _upside;
+        face _downside;
+
+        public face upside
+        {
+            get { return _upside; }
+            set { _upside = value; }
+        }
+
+        public face downside
+        {
+            get { return _downside; }
+            set { _downside = value; }
+        }
+
+        public Coin()
+        {
+            upside = face.Heads;
+            downside = face.Tails;
+        }
+
+        
+    }
+}
