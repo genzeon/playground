f2ccbbf8e06e58deccf29d671e245dc6e1e4d3c6
AShaik <AShaik@HYD-ASHAIK>
2022-07-25 18:24:10 +0530

    Coin Stimulate Entity

---
+++ AShaik/projects/webentity/coinstimulate/CoinClass/Coin.cs
@@ -0,0 +1,115 @@
+﻿
+
+namespace coinstimulate.Models
+{
+
+
+    public enum Coinface
+    {
+        heads, tails
+    }
+
+    public sealed class coin
+    {
+
+        private int _toss;
+        private Coinface _up;
+        private Coinface _down;
+        private int _headcount;
+        private int _tailcount;
+        private static coin _instance;
+
+
+
+        public int Toss
+        {
+            get
+            {
+                return _toss;
+
+
+            }
+
+            set
+            {
+                _toss = value;
+            }
+        }
+        public Coinface Up
+        {
+            get { return _up; }
+            set { _up = value; }
+
+
+        }
+        public Coinface Down
+        {
+
+            get { return _down; }
+            set { _down = value; }
+        }
+
+        public int Headcount
+        {
+            get { return _headcount; }
+            set { _headcount = value; }
+        }
+        public int Tailcount
+        {
+            get { return _tailcount; }
+            set { _tailcount = value; }
+        }
+        public static coin Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new coin();
+                }
+
+
+                return _instance;
+            }
+
+        }
+
+        private coin()
+        {
+            Up = Coinface.heads;
+            Down = Coinface.tails;
+        }
+        public override string ToString()
+        {
+            //string str = $"up= {this.Up} Down={this.Down}";
+            string str = this.Up.ToString();
+            return str;
+        }
+
+        public void flip()
+        {
+            Random random = new Random();
+            int num = random.Next(2);
+
+            if (num == 0)
+            {
+                Up = Coinface.heads;
+                Headcount++;
+                Down = Coinface.tails;
+
+            }
+            else
+            {
+
+                Up = Coinface.tails;
+                Tailcount++;
+                Down = Coinface.heads;
+            }
+
+            Toss++;
+
+        }
+
+    }
+
+}
---
+++ AShaik/projects/webentity/coinstimulate/coinstimulate/Models/Coindb.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace coinstimulate.Models
+{
+    public class Coindb
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public int FaceUp { get; set; } 
+        public int TossCount { get; set; }  
+         
+    }
+}
